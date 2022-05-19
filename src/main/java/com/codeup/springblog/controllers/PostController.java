9aa7469c46362772b8c72d4628179f9ad38a5451
Shaz Soto-Munet <shaz.soto.munet@gmail.com>
2022-05-19 11:39:36 -0500

    Finalize Relationships exercise.

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -3,8 +3,10 @@ package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Coffee;
 import com.codeup.springblog.models.Post;
+import com.codeup.springblog.models.User;
 import com.codeup.springblog.repositories.AdsRepository;
 import com.codeup.springblog.repositories.PostRepository;
+import com.codeup.springblog.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,16 +18,21 @@ import java.util.List;
 @RequestMapping("/posts")
 public class PostController {
 
-    private final PostRepository postDao;
+    private final PostRepository postsDao;
+    private final UserRepository usersDao;
 
-    public PostController(PostRepository postDao){
-        this.postDao = postDao;
+    public PostController(PostRepository postDao, UserRepository usersDao){
+        this.postsDao = postDao;
+        this.usersDao = usersDao;
     }
+
+
 
 
     @GetMapping("/index")
     public String index(Model model){
 //        Lecture code for hard-coded posts commented out below:
+
 //        ArrayList<Post> posts = new ArrayList<>();
 //        Post post = new Post("Hello World", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 //        Post post2 = new Post("Hello World... again!", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
@@ -34,18 +41,21 @@ public class PostController {
 //        posts.add(post2);
 //
 //        model.addAttribute("posts", posts);
-        List<Post> posts = postDao.findAll();
+
+
+        List<Post> posts = postsDao.findAll();
         model.addAttribute("posts", posts);
 
         return "posts/index";
     }
 
     @GetMapping("/{id}")
-    public String postByID(@PathVariable int id, Model model){
-        Post post = new Post("Hello World, Id: " + id, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+    public String postByID(@PathVariable long id, Model model){
+//        Post post = new Post("Hello World, Id: " + id, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
+        Post post = postsDao.getById(id);
         model.addAttribute("post", post);
-        model.addAttribute("id", id);
+
 
         return "posts/show";
     }
@@ -55,17 +65,22 @@ public class PostController {
         return "posts/create";
     }
 
+
     @PostMapping("/create")
     public String createPost(
             @RequestParam(name="title") String title,
             @RequestParam(name="body") String body)
     {
 
-        Post post = new Post(title, body);
-        postDao.save(post);
+        User user = usersDao.findAll().get(0);
+
+        Post post = new Post(title, body, user);
+        postsDao.save(post);
 
         return "redirect:/posts/index";
 
     }
+
+
 
 }
--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -8,18 +8,28 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     @Column(nullable = false, length = 225)
     private String title;
+
     @Column(nullable = false)
     private String body;
 
-    public Post() {
-    }
+    @ManyToOne
+    @JoinColumn (name="user_id")
+    private User user;
 
-    public Post(String title, String body) {
+
+    public Post() {}
+
+
+    public Post(String title, String body, User user) {
         this.title = title;
         this.body = body;
+        this.user = user;
     }
+
+
 
     public String getTitle() {
         return title;
@@ -39,6 +49,13 @@ public class Post {
 
     public long getId() { return id; }
 
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
 
 
